df5a099f0c43c3a45af9ab15c7bee2d8eb258f66
Eduardo Avelar <eavelardev@gmail.com>
2020-02-09 19:25:46 -0800

    first version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # learn-answers
 Program to learn the answers of a set of questions
+
+I think that one of the best ways to learn new things y through repetion, for that reason I did this program for learn the answers of a set of questions in a fun way, importing that information from a JSON.
+
+## Using
+
+```
+python learn_answers.py --file <my source code file>
+```
+
+example:
+
+```
+python learn_answers.py --file handson-ml2/chapter1_The\ Machine\ Learning\ Landscape.json
+```
+
+Bug: The program crash when you use a small terminal size.
---
+++ learn_answers.py
@@ -0,0 +1,117 @@
+import argparse
+import json
+import curses
+
+x = y = 0
+
+def main():
+    parser = argparse.ArgumentParser(description='Learn answers.')
+
+    parser.add_argument('-f', '--file',
+                        type=argparse.FileType('r'),
+                        help='Input file.')
+
+    clo = '>>> '
+    cli = '<<< '
+    info = '--> '
+    args = parser.parse_args()
+    f = args.file
+    data = json.load(f)
+    f.close()
+    curses.initscr()
+    win = curses.initscr()
+    num_rows, num_cols = win.getmaxyx()
+
+    def win_print(value):
+        global x, y
+        win.addstr(y, x, value)
+        x += len(value)
+
+    def win_println(value):
+        win_print(value)
+        global x, y
+        x = 0
+        y += 1
+
+    def get_word():
+        word = ''
+        while True:
+            ch = win.getkey()
+            if ch == ' ' or ch == '\n':
+                return word, ch
+            if ch.isprintable():
+                word += ch
+
+    for question in data:
+        while True:
+            win.clear()
+            global x, y
+            x = y = 0
+            win_println(clo + question['question'])
+            answer = question['answer']
+            answer_words = question['answer'].split()
+
+            win_println(cli) 
+            in_word, state = get_word()
+
+            if state == '\n':
+                if len(in_word) > 0:
+                    continue
+                else:
+                    break
+
+            correct = None
+            restart_question = False
+            for i in range(len(answer_words)):
+
+                if in_word != answer_words[i]:
+                    while True:
+                        if correct is None:
+                            win_println(info + answer)
+                            win_println(info + 'Practice more!')
+                            correct = False
+                        else:
+                            win_print(info) 
+                            for j in range(i):
+                                x += len(answer_words[j]) + 1
+                            win_println(answer_words[i])
+
+                        win_print(cli) 
+
+                        for j in range(i):
+                            win_print(answer_words[j] + ' ')
+                        win_println('')
+                        
+                        in_word, state = get_word()
+
+                        if state == '\n':
+                            restart_question = True
+                            break
+
+                        if in_word == answer_words[i]:
+                            break
+
+                if correct is None and i == len(answer_words) - 1:
+                    correct = True
+                    break
+
+                if restart_question:
+                    break
+
+                in_word, state = get_word()
+
+                if state == '\n' and i < len(answer_words) - 2:
+                    break
+
+            if correct:
+                break
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+
+    print('\nTraining finished1') 
+    curses.endwin()
